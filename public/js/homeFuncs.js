365da4daaa08eac8cc921e3b580aa781dcee0133
Colby Medeiros <18205176+colbymed@users.noreply.github.com>
2021-12-22 00:46:48 +0000

    Added button logic when selecting team

--- public/js/homeFuncs.js
+++ public/js/homeFuncs.js
@@ -1,7 +1,17 @@
 $("#years").on("change", getTeams);
+$("#teams").on("change", setButton);
 
-$('#teams').css("display", "none");
-$('#button').css("display", "none");
+$('#teams').hide();
+$('#button').hide();
+
+function setButton() {
+    if ($("#teams").val() == -1) {
+        $("#button").prop("disabled", true);
+    }
+    else {
+        $("#button").prop("disabled", false);
+    }
+}
 
 async function getTeams() {
     let year = $("#years").val();
@@ -9,18 +19,19 @@ async function getTeams() {
         let url = `https://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code='mlb'&all_star_sw='N'&sort_order=name_asc&season='${year}'&team_all_season.col_in=name_display_full&team_all_season.col_in=team_id`;
         let data = await fetchData(url);
 
-        $("#teams").html(`<option value='-1'> Select a Team </option>`);
+        $("#teams").html(`<option selected value='-1'> Select a Team </option>`);
         for (i = 0; i < data.team_all_season.queryResults.row.length; i++) {
             let teamID = data.team_all_season.queryResults.row[i].team_id;
             let teamName = data.team_all_season.queryResults.row[i].name_display_full;
             $("#teams").append(`<option value="${teamID}"> ${teamName} </option>`);
         }
-        $('#teams').css("display", "block");
-        $('#button').css("display", "block");
+        $('#teams').show();
+        $('#button').show();
+        setButton();
     }
     else {
-        $('#teams').css("display", "none");
-        $('#button').css("display", "none");
+        $('#teams').hide();
+        $('#button').hide();
     }
 }
 async function fetchData(url) {
