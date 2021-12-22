d54f72de4267d4011678b3814fbe4795077b523d
Colby Medeiros <18205176+colbymed@users.noreply.github.com>
2021-12-22 05:24:17 +0000

    code formatting

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const fetch = require('node-fetch');
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-   res.render('home');
+app.get('/', async (req, res) => {
+    res.render('home');
 });
 app.get('/team/info', async (req, res) => {
     let teamID = req.query.teamID;
@@ -27,14 +27,14 @@ app.get('/team/info', async (req, res) => {
     }
     else {
         //console.log(rows.roster_team_alltime.queryResults.row[0]);
-        res.render('roster', {"players" : rows.roster_team_alltime.queryResults.row, "year" : year, "teamName" : teamName});
+        res.render('roster', { "players": rows.roster_team_alltime.queryResults.row, "year": year, "teamName": teamName });
     }
 });
 app.listen(3000, () => {
-   console.log('server started');
+    console.log('server started');
 });
 async function fetchData(url) {
-  let response = await fetch(url);
-  let data = await response.json();
-  return data;
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
 }
